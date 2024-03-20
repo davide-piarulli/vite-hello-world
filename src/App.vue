6d2de5ceba8346d97141e28eb5ed9a9e0cdce32f
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-20 14:13:42 +0000

    [add] content and style

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script>
 export default {
-  
+  data(){
+    return {
+      title: 'Il mio primo progetto con VITE!',
+      description:'Questo è il mio primo progetto creato da linea di comando grazie a VITE.'
+    }
+  }
 }
 </script>
 
 <template>
   <div>
-    <h1></h1>
-    <p></p>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
   </div>
 </template>
 
 <style scoped>
-
+  h1{
+    color:red;
+  }
 </style>
